e692dee3981bf56ec82816ef541e7ef778949b2f
trucane <trufashionista2013@gmail.com>
2019-09-26 17:46:31 -0500

    4th commit

--- src/Components/Menu/TopBar.js
+++ src/Components/Menu/TopBar.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {styles} from '../resource/stylesWith';
-import {withStyles} from '@material-ui/styles'
-import {
-    AppBar,
-    Toolbar,
-    IconButton,
-    Button,
-    Typography,
-  } from "@material-ui/core";
-  import MenuIcon from "@material-ui/icons/Menu";
+import {withStyles} from '@material-ui/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
 
 
 
