438286ea3e9e6783a20f1799b286d3fcac569054
trucane <trufashionista2013@gmail.com>
2019-09-26 17:50:28 -0500

    5th commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,11 @@
 import React from "react";
-
-
 import TopBar from './Menu/TopBar';
-import Footer from './Menu/Footer';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="App">
        <TopBar />
-       <Footer />
-
       </div>
     );
   }
